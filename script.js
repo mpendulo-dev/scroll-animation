ad38937d545f81670066a74898404c71a90f9b2e
Mpendulo Philip Sibiya <79789895+mpendulo-dev@users.noreply.github.com>
2021-03-11 16:37:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // funcionality
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll',checkBoxes);
+
+// function call
+checkBoxes();
+
+function checkBoxes() {
+    
+    // innerHeight property returns the height of a window's content area
+    const trigger = window.innerHeight/5 * 4;
+
+    // looping through the box array list
+    boxes.forEach(box => {
+
+        // getBoundingClientRect() method returns the size of an element, in this case which is the top of the viewport
+        const boxTop = box.getBoundingClientRect().top;
+
+        if(boxTop < trigger) {
+            box.classList.add('show');
+        } else {
+            box.classList.remove('show');
+        }
+    });
+
+
+}
